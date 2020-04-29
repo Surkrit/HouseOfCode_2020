95f9a909bc4a107ce9d1ee3e98978cb0149b96f7
Surkrit <jensendennis520@gmail.com>
2020-04-29 23:15:47 +0200

    House of Code opgave 2020

---
+++ HouseOfCode_2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HouseOfCode_2020.MvcApplication" Language="C#" %>
---
+++ HouseOfCode_2020/Models/OurDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HouseOfCode_2020.Models
+{
+    public class OurDbContext : DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+    }
+}
